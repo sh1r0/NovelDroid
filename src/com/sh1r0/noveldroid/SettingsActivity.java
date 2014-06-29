116c847772a3fef08ec0bf0ef5fbc56b9dfc424c
sh1r0 <sh1r0@users.noreply.github.com>
2014-06-30 00:28:13 +0800

    updated android support libraries

--- src/com/sh1r0/noveldroid/SettingsActivity.java
+++ src/com/sh1r0/noveldroid/SettingsActivity.java
@@ -1,21 +1,17 @@
 package com.sh1r0.noveldroid;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
 public class SettingsActivity extends ActionBarActivity {
-	private static final int contentViewCompatID = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) ? android.R.id.content
-			: R.id.action_bar_activity_content;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		getSupportFragmentManager().beginTransaction().replace(contentViewCompatID, new SettingsFragment()).commit();
+		getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
 	}
 
 	@Override
